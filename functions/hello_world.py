0ead08119e763bf264de0513d173587c6205566e
singhmaninder <singhmaninder1001@gmail.com>
2019-09-08 10:36:26 +0530

    basic python function

---
+++ functions/hello_world.py
@@ -0,0 +1,15 @@
+# Define a function
+def say_hello():
+    # block belonging to the function.
+    print ('Hello World')
+# End of function
+
+say_hello() # call the function
+say_hello() # call the function again
+
+
+# OUTPUT
+# python functions_basics.py
+# Hello World
+# Hello World
+
