10ba99df533d88d8b1780e58f664c7c45a9adb90
singhmaninder <singhmaninder1001@gmail.com>
2019-10-02 14:01:19 +0530

    added basic working of modules and list.

---
+++ modules/module_using_sys.py
@@ -0,0 +1,13 @@
+import sys
+
+print('The command line arguments are: ')
+for arg in sys.argv:
+    print arg
+
+print '\n\nThe PYTHONPATH is', sys.path
+
+
+if __name__ == '__main__':
+    print '\nThe program is being run by itself.'
+else:
+	print 'I am being imported from another module.'
