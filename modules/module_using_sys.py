5affc0116d9ce2dd40522dfb3c2486e498f152e4
singhmaninder <singhmaninder1001@gmail.com>
2019-10-06 10:35:09 +0530

    migrate the code to python 3' '

--- modules/module_using_sys.py
+++ modules/module_using_sys.py
@@ -2,12 +2,12 @@ import sys
 
 print('The command line arguments are: ')
 for arg in sys.argv:
-    print arg
+    print (arg)
 
-print '\n\nThe PYTHONPATH is', sys.path
+print ('\n\nThe PYTHONPATH is', sys.path)
 
 
 if __name__ == '__main__':
-    print '\nThe program is being run by itself.'
+    print ('\nThe program is being run by itself.')
 else:
-    print 'I am being imported from another module.'
+    print ('I am being imported from another module.')
